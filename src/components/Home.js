8ff3cd3b1945547ee7c3856e831d5cf0031ab206
brunol009 <bruno.letelle.91@gmail.com>
2022-06-30 21:11:06 -0300

    pokeload added

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,24 @@ export default function Home(){
 
     const [show, setShow] = useState(false)
 
+    function handleOnClick(){
+        setShow(true)
+    }
+
     return(
         <div className="home">
-            { show &&
+        { show ?    
             <div className="card-list">
                 <Card className="card"/>
                 <Card className="card"/>
                 <Card className="card"/>
-            </div>}
+            </div>
+            :
+            <div className="home-welcome">
+                <h1>Bienvenido entrenador</h1>
+                <button onClick={handleOnClick}>Conoce a tu equipo</button>
+            </div>
+        }
         </div>
     )
 }
